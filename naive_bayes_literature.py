c34e969a43e97efb9e8f7be9b2073fd3ffa84f26
saospo <sao47@georgetown.edu>
2020-05-11 00:45:02 -0400

    lots and lots of changes, oh boy:
    util.py:
    stopword_counts is now just very simple, very blunt. takes in an
    iterable of words (this is because nltk.corpus.gutenberg uses a class
    that isn't list or anything else, but can be iterated over) and returns
    a defaultdict with counts of stopwords.
    
    naive_bayes_literature.py:
    changed processing of Our Times, by Hemingway, to reflect new behavior
    of stopword_counts()
    
    moby dick is now processed into a functional defaultdict with stopword
    counts

--- naive_bayes_literature.py
+++ naive_bayes_literature.py
@@ -1,28 +1,28 @@
 import argparse
 import PyPDF2 as pdf
 from util import *
+from nltk.corpus import gutenberg
+
 
 def main():
+    ### HEMINGWAY -- OUR TIMES ###
     hemingway_url = "https://www.gutenberg.org/files/61085/61085-0.txt"
-
     ourtimes = pull_gutenberg(hemingway_url)
-
-    print(len(ourtimes))
-
     # clears everything after the end of the final chapter (e.g. gutenberg licensing, text from back of book)
-    chapters = ourtimes.split("Here ends _The Inquest_")
-
-    chapters = chapters[0].split("chapter")
-
-    chapters = chapters[1:]
-    chapters = [chapter.strip() for chapter in chapters]
-
-    # should be 18
-    print(len(chapters))
-
+    # and before the first
+    # then tokenizes the result
+    chapters = word_tokenize(ourtimes[ourtimes.find("chapter 1"):ourtimes.find("Here ends _The Inquest_")])
     hemingway_ourtimes = stopword_counts(chapters)
-
     print(hemingway_ourtimes)
+    ##############################
+
+    ### MELVILLE -- MOBY DICK ###
+    white_whale = gutenberg.words("melville-moby_dick.txt")
+    print(len(white_whale))
+    # need to just .join here because white_whale ends up being a list of words
+    # if it's a string then at least word_tokenize() will work on it in stopword_counts()
+    melville_mobydick = stopword_counts(white_whale)
+    print(melville_mobydick)
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 from urllib import request
 
 from nltk import word_tokenize
-from nltk.corpus import gutenberg, stopwords
+from nltk.corpus import stopwords
 from collections import defaultdict
 
 
@@ -19,49 +19,20 @@ def pull_gutenberg(url):
 # and declarative)
 # second flag determines whether the return is a list of defaultdicts going by chapter or a
 # single defaultdict of stop words for the whole work
-def stopword_counts(chapters, punctuation=False, by_chapters=False):
-    """gives counts of all stopwords in all chapters of a book.
+def stopword_counts(chapters, punctuation=False):
+    """gives counts of all stopwords in a iterable of words.
         can set flag to give counts of punctuation as well.
-        third tag allows return to separate stopword counts by chapter.
-
-        DO NOT PASS A STRING TO THIS: CHAPTERS MUST BE AN ITERABLE, EVEN IF
-        LEN(CHAPTERS) == 1
         """
-    # set var which will become return value
-    stops = 0
 
     stop_words = set(stopwords.words('english'))
 
     if punctuation:
         print("right now this does nothing, but maybe it will later\n")
 
-    # if the flag is set to give a return separated by chapters
-    if by_chapters:
-        stops_by_chapter = []
+    stops_in_work = defaultdict(int)
 
-        for index in range(0, len(chapters)):
+    for word in chapters:
+        if word in stop_words:
+            stops_in_work[word] += 1
 
-            temp_stops_in_chapter = defaultdict(int)
-
-            chapter = word_tokenize(chapters[index])
-            for word in chapter:
-                if word in stop_words:
-                    temp_stops_in_chapter[word] += 1
-
-            stops_by_chapter.append(temp_stops_in_chapter)
-
-        # assign return value the list of stopword defaultdicts
-        stops = stops_by_chapter
-    # otherwise, i.e. whole book, return the entire work of stopword counts together
-    else:
-        stops_in_work = defaultdict(int)
-
-        for index in range(0, len(chapters)):
-            chapter = word_tokenize(chapters[index])
-            for word in chapter:
-                if word in stop_words:
-                    stops_in_work[word] += 1
-
-        stops = stops_in_work
-
-    return stops
+    return stops_in_work
