504ad0ed1a397e5f0fd031757d69c7a55a1671e4
saospo <sao47@georgetown.edu>
2020-05-11 02:07:16 -0400

    added a bunch of new sources.
    word lengths are getting printed to figure out if some of the longer
    ones should be broken down

--- naive_bayes_literature.py
+++ naive_bayes_literature.py
@@ -11,9 +11,10 @@ def main():
     # clears everything after the end of the final chapter (e.g. gutenberg licensing, text from back of book)
     # and before the first
     # then tokenizes the result
-    chapters = word_tokenize(ourtimes[ourtimes.find("chapter 1"):ourtimes.find("Here ends _The Inquest_")])
-    hemingway_ourtimes = stopword_counts(chapters)
-    print(hemingway_ourtimes)
+    ourtimes = word_tokenize(ourtimes[ourtimes.find("chapter 1"):ourtimes.find("Here ends _The Inquest_")])
+    print(len(ourtimes))
+    hemingway_ourtimes = stopword_counts(ourtimes)
+    print("hemingway_ourtimes\n", hemingway_ourtimes)
     ##############################
 
     ### MELVILLE -- MOBY DICK ###
@@ -22,7 +23,49 @@ def main():
     # need to just .join here because white_whale ends up being a list of words
     # if it's a string then at least word_tokenize() will work on it in stopword_counts()
     melville_mobydick = stopword_counts(white_whale)
-    print(melville_mobydick)
+    print("melville_mobydick\n", melville_mobydick)
+    ##############################
+
+    ### MELVILLE -- THE APPLE TREE TABLE ###
+    bartleby_url = "http://www.gutenberg.org/cache/epub/11231/pg11231.txt"
+    bartleby = pull_gutenberg(bartleby_url)
+    startindex = bartleby.find("I am a rather elderly man.")
+    endindex = bartleby.find("End of Project Gutenberg")
+    bartleby = word_tokenize(bartleby[startindex:endindex])
+    print(len(bartleby))
+    melville_bartleby = stopword_counts(bartleby)
+    print("melville_bartleby\n", melville_bartleby)
+    ##############################
+
+    ### MELVILLE -- PIERRE ###
+    pierre_url = "http://www.gutenberg.org/cache/epub/34970/pg34970.txt"
+    pierre = pull_gutenberg(pierre_url)
+    startindex = pierre.find("There are some strange summer mornings in the country")
+    endindex = pierre.find("End of Project Gutenberg")
+    pierre = word_tokenize(pierre[startindex:endindex])
+    print(len(pierre))
+    melville_pierre = stopword_counts(pierre)
+    print("melville_pierre\n", melville_pierre)
+    ##############################
+
+    ### CARROLL -- ALICE IN WONDERLAND ###
+    wonderland = gutenberg.words("carroll-alice.txt")
+    print(len(wonderland))
+    # need to just .join here because white_whale ends up being a list of words
+    # if it's a string then at least word_tokenize() will work on it in stopword_counts()
+    carroll_alice = stopword_counts(wonderland)
+    print("carroll_alice\n", carroll_alice)
+
+    ##############################
+
+    ### MILTON -- PARADISE LOST ###
+    paradise = gutenberg.words("milton-paradise.txt")
+    print(len(paradise))
+    # need to just .join here because white_whale ends up being a list of words
+    # if it's a string then at least word_tokenize() will work on it in stopword_counts()
+    milton_paradise = stopword_counts(paradise)
+    print("milton_paradise\n", milton_paradise)
+    ##############################
 
 
 if __name__ == "__main__":
