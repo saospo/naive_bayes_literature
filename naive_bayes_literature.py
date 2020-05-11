fceec0e50a09f05d7e2d1d28bd7dd0fa0eeb5b23
saospo <sao47@georgetown.edu>
2020-05-10 22:49:19 -0400

    reorganized some stuff.
    put a lot of the processing for Our Times in main.

--- naive_bayes_literature.py
+++ naive_bayes_literature.py
@@ -1,12 +1,34 @@
 import argparse
 import PyPDF2 as pdf
-from util.py import *
+from util import *
 
 def main():
+    hemingway_url = "https://www.gutenberg.org/files/61085/61085-0.txt"
+
+    ourtimes = pull_gutenberg(hemingway_url)
+
+    print(len(ourtimes))
+
+    # clears everything after the end of the final chapter (e.g. gutenberg licensing, text from back of book)
+    chapters = ourtimes.split("Here ends _The Inquest_")
+
+    chapters = chapters[0].split("chapter")
+
+    chapters = chapters[1:]
+    chapters = [chapter.strip() for chapter in chapters]
+
+    # should be 18
+    print(len(chapters))
+
+    hemingway_ourtimes = stopword_counts(chapters)
+
+    print(len(hemingway_ourtimes))
+
+    print(hemingway_ourtimes[0])
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
 
-    main(args.sts_train_file, args.sts_dev_file, args.w2v_file)
+    main()
--- util.py
+++ util.py
@@ -35,27 +35,3 @@ def stopword_counts(chapters, punctuation = False):
         stops_by_chapter.append(temp_stops_in_chapter)
 
     return stops_by_chapter
-
-
-hemingway_url = "https://www.gutenberg.org/files/61085/61085-0.txt"
-
-ourtimes = pull_gutenberg(hemingway_url)
-
-print(len(ourtimes))
-
-# clears everything after the end of the final chapter (e.g. gutenberg licensing, text from back of book)
-chapters = ourtimes.split("Here ends _The Inquest_")
-
-chapters = chapters[0].split("chapter")
-
-chapters = chapters[1:]
-chapters = [chapter.strip() for chapter in chapters]
-
-# should be 18
-print(len(chapters))
-
-hemingway_ourtimes = stopword_counts(chapters)
-
-print(len(hemingway_ourtimes))
-
-print(hemingway_ourtimes[0])
